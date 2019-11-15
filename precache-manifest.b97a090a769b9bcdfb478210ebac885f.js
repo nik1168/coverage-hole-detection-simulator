e778bb1c006645ab9b4e1e8323d335250eb95a84
nik1168 <ngeisser32@gmail.com>
2019-11-15 21:19:19 +0100

    Updates

---
+++ precache-manifest.b97a090a769b9bcdfb478210ebac885f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc94b9677cf085d360c6cf4112be5a6c",
+    "url": "/coverage-hole-detection-simulator/index.html"
+  },
+  {
+    "revision": "4e89acaad7a163a92f50",
+    "url": "/coverage-hole-detection-simulator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8c199dcdb163af3c016b",
+    "url": "/coverage-hole-detection-simulator/static/js/2.c8fcee76.chunk.js"
+  },
+  {
+    "revision": "4e89acaad7a163a92f50",
+    "url": "/coverage-hole-detection-simulator/static/js/main.a123e2ea.chunk.js"
+  },
+  {
+    "revision": "f785466e262efffc3cbf",
+    "url": "/coverage-hole-detection-simulator/static/js/runtime-main.429f0b50.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/coverage-hole-detection-simulator/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coverage-hole-detection-simulator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/coverage-hole-detection-simulator/static/media/shape.208f3d38.svg"
+  }
+]);
